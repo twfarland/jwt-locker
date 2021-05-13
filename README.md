c287c8ce216acb54adab9c5966ede6942f1d7f5c
Tim Farland <twfarland@gmail.com>
2021-05-13 18:37:54 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # jwt-locker
-Secure and user-friendly client-side jwt storage
+
+### Secure and user-friendly client-side jwt storage.
+
+I made this library because I've come into many projects that get browser side usage of jwt wrong, both in security and in UX. People are storing tokens
+in localStorage, which is prone to xss attacks, and refresh token handling
+is often fumbled. Even AWS's own Amplify library doesn't get it right
+as of now.
+
+I read this excellent article: [The Ultimate Guide to handling JWTs on frontend clients (GraphQL)](https://hasura.io/blog/best-practices-of-using-jwt-with-graphql) and have adapted its recommendations for a more traditional rest flow.
+
+To use this library, you pass it your implementation of login, logout, signup, and refresh token functions, and specify your auth api, and and apis that consume the auth. jwt-locker will wire it all up so the correct auth state is in play, refreshing.
+
+Certain assumptions about the serverside implementation are made. For example you will need the login endpoint to set a refresh token cookie, and the refresh token endpoint will need to read from it. See this reference server implementation for more info.
+
+Features
+
+- Token is stored in memory, no localStorage (for xss protection)
+- Seamless silent refresh
+- During token refresh, outbound calls will be delayed until refresh is finished
+- Auth api can be separated from protected apis
+- Support short-lived jwt tokens with long-lived refresh tokens
+
+Examples
+
+- Axios basic
+- Axios / React
+- Axios / Svelte
+
+Related repos
+
+- Reference server
+- Axios adapter
---
+++ src/index.ts
@@ -0,0 +1,165 @@
+export interface TokenSet {
+  jwtToken: string;
+  jwtTokenExpiry: string;
+}
+
+export type Interceptor<ApiInstance> = (
+  api: ApiInstance,
+  interceptor: () => Promise<any>
+) => () => void; // returns unbinder
+
+export interface JwtConnectConfig<ApiInstance> {
+  protectedApis: ApiInstance[];
+  signup(email: string, password: string): Promise<void>;
+  login(email: string, password: string): Promise<TokenSet>;
+  logout(): Promise<void>;
+  refreshToken(): Promise<TokenSet>;
+  setAuthHeader(api: ApiInstance, value: string): void;
+  setRefreshingInterceptor: Interceptor<ApiInstance>;
+  setUnauthorizedInterceptor: Interceptor<ApiInstance>;
+  redirectToLogin: () => void;
+  REFRESH_CHECK_INTERVAL?: number;
+  REFRESH_DELAY_INTERVAL?: number;
+}
+
+export default function jwtConnect<ApiInstance>({
+  protectedApis,
+  signup,
+  login,
+  logout,
+  refreshToken,
+  setAuthHeader,
+  setRefreshingInterceptor,
+  setUnauthorizedInterceptor,
+  redirectToLogin,
+  REFRESH_CHECK_INTERVAL = 60000,
+  REFRESH_DELAY_INTERVAL = 100,
+}: JwtConnectConfig<ApiInstance>) {
+  let tokenSet: TokenSet | null = null;
+
+  let unBindApis: (() => void) | undefined = undefined;
+
+  let refreshing = false;
+
+  async function handleSignup(email: string, password: string) {
+    await signup(email, password);
+    redirectToLogin();
+  }
+
+  async function handleLogin(email: string, password: string) {
+    tokenSet = await login(email, password);
+    unBindApis = bindApis();
+  }
+
+  async function handleRefreshToken() {
+    if (refreshing) {
+      return;
+    }
+    refreshing = true;
+    try {
+      tokenSet = await refreshToken();
+      unBindApis = bindApis();
+    } catch (err) {
+      redirectToLogin();
+    }
+    refreshing = false;
+  }
+
+  function bindApis() {
+    if (unBindApis) {
+      unBindApis();
+    }
+
+    const refreshCheckInterval = setInterval(
+      refreshCheck,
+      REFRESH_CHECK_INTERVAL
+    );
+
+    const apiUnBinders = protectedApis.map(bindApi);
+
+    return function unBindApis() {
+      clearInterval(refreshCheckInterval);
+
+      for (const apiUnBind of apiUnBinders) {
+        apiUnBind();
+      }
+    };
+  }
+
+  function bindApi(api: ApiInstance) {
+    if (!tokenSet) {
+      throw new Error("Missing token");
+    }
+
+    // add auth to headers
+    setAuthHeader(api, tokenSet?.jwtToken);
+
+    // intercept requests, if refreshing in progress, delay them until it is done
+    const unSetRefreshingInterceptor = setRefreshingInterceptor(
+      api,
+      refreshDelayer
+    );
+
+    // add interceptors to check for expired token
+    const unSetUnauthorizedInterceptor = setUnauthorizedInterceptor(
+      api,
+      handleRefreshToken
+    );
+
+    return function unBindApi() {
+      setAuthHeader(api, "");
+      unSetRefreshingInterceptor();
+      unSetUnauthorizedInterceptor();
+    };
+  }
+
+  async function refreshCheck() {
+    if (
+      !tokenSet ||
+      new Date().valueOf() + REFRESH_CHECK_INTERVAL >
+        new Date(tokenSet.jwtTokenExpiry).valueOf() // expiring in < 1 min
+    ) {
+      await handleRefreshToken();
+    }
+  }
+
+  async function refreshDelayer() {
+    if (!refreshing) {
+      return Promise.resolve(tokenSet?.jwtToken);
+    } else {
+      return new Promise((resolve) => {
+        const checkRefreshInterval = setInterval(() => {
+          if (!refreshing) {
+            clearInterval(checkRefreshInterval);
+            resolve(tokenSet?.jwtToken);
+          }
+        }, REFRESH_DELAY_INTERVAL);
+      });
+    }
+  }
+
+  async function handleLogout() {
+    tokenSet = null;
+    window.localStorage.setItem("logout", String(Date.now()));
+    if (unBindApis) {
+      unBindApis();
+    }
+    await logout();
+  }
+
+  function syncLogoutAcrossTabs({ key }: StorageEvent) {
+    if (key === "logout") {
+      logout();
+    }
+  }
+
+  window.addEventListener("storage", syncLogoutAcrossTabs);
+
+  return {
+    refreshToken: handleRefreshToken,
+    login: handleLogin,
+    logout: handleLogout,
+    signup: handleSignup,
+    getToken: () => tokenSet,
+  };
+}
